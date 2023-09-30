d3300a51e74ffe7f582006f44f3d9324f70448e2
anisul islam <anisulislam980340@gmail.com>
2023-09-30 22:20:25 +0600

    class and object

---
+++ class_object.py
@@ -0,0 +1,15 @@
+# class
+class Phone:
+    name= "itel"
+    price=6500
+    year=2
+
+
+# object
+my_phone = Phone()
+
+
+# print object
+print(my_phone)
+print(my_phone.name)
+print(my_phone.year)
